396888494403fb6434316bb7545fbff25817729d
Xiao Shi (Dean) <deanshi0626@hotmail.com>
2014-09-05 11:44:17 -0700

    Update run-server.sh

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -i -d -p 3306:3306 -p 80:80 -p 22:22 -v /data/phabricator/mysql:/var/lib/mysql -v /data/phabricator/repo:/var/repo -v /data/phabricator/conf:/opt/phabricator/conf yesnault/docker-phabricator
+docker run -i -d -p 3306:3306 -p 80:80 -p 22:22 -v /data/phabricator/mysql:/var/lib/mysql -v /data/phabricator/repo:/var/repo -v /data/phabricator/conf:/opt/phabricator/conf phabricator
 
